7e73efe32a2643fa01946213b0e5c15e29c82505
Yu-Chun Lo <howard.lo@nlplab.cc>
2017-07-21 17:57:24 +0800

    Add url content crawler for twitter data

---
+++ crawlers/twitter_url_content_crawler.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import pickle
+import pandas as pd
+sys.path.append('../data_helpers')
+
+from tqdm import tqdm
+from datetime import date, timedelta
+from twitter_data_helper import TwitterDataHelper
+from newspaper import Article
+
+def get_url_content(url):
+    print('Downloading "{}"'.format(url))
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        return article
+    except: return {}
+
+dataset_folder_path = '../dataset/'
+
+if __name__ == '__main__':
+
+    # Get data range
+    start = date(2000, 1, 1)
+    end = date.today()
+    delta = end - start
+    date_range = [(start + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(delta.days + 1)]
+
+    # Get data
+    data_helper = TwitterDataHelper()
+    df = data_helper.get_data(date_range=date_range)
+
+    print('* {} rows retrieved.'.format(len(df)))
+    print('* Prepare to download url content for each row...')
+
+    # Extract URL
+    df['url'] = df['raw_data'].apply(lambda raw_data: raw_data.entities \
+                                                              .get('urls', [{}])[0] \
+                                                              .get('expanded_url', ''))
+
+    # For every URL, extract article
+    data_file_path = os.path.join(dataset_folder_path, 'twitter_data_{}.pkl'.format(len(df)))
+
+    df['url_content_raw_data'] = [{} for _ in range(len(df))]
+
+    for i, url in tqdm(enumerate(df['url'])):
+        # Crawl url content
+        df['url_content_raw_data'][i] = get_url_content(url)
+        # Cache
+        if i % 5 == 0:
+            print('* Caching...')
+            df.to_pickle(data_file_path)
+
+        if i == 10:
+            break
+
+    print('* Saving...')
+    df.to_pickle(data_file_path)
+    print('* Done.')
